d3affda82d9b1f6565b1ad228bbeefe960b9624a
Gustavo Sotero <gustavo.sotero13@gmail.com>
2025-05-08 17:01:41 -0300

    feat: adicionar lógica para criação de usuário padrão e novo arquivo de testes HTTP

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,24 @@
 import { serve } from '@hono/node-server';
-import { Hono } from 'hono';
-
-const app = new Hono();
+import { hash } from 'bcrypt';
+import { app } from './app';
+import { prisma } from './lib/db';
 
 // Iniciar servidor
 const port = process.env.PORT || 3000;
 console.log(`Servidor rodando na porta ${port}`);
+const usuariosCount = await prisma.usuario.count();
+if (usuariosCount === 0) {
+	// Criar um usuário padrão
+	await prisma.usuario.create({
+		data: {
+			nome: 'Admin',
+			matricula: '1',
+			senha: await hash('admin', 10),
+			setor: 'Administração',
+			autoridade: 'ADMIN'
+		}
+	});
+}
 serve({
 	fetch: app.fetch,
 	port: Number(port)
